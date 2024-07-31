aecf82f0e30ad483aa73f8282a87b02d27ea4e82
hendisantika <hendisantika@yahoo.co.id>
2024-07-31 17:56:07 +0700

    Modify dashboard page.tsx & ui/dashboard revenue-chart.tsx🫸🌀✏️📗🐧🐳

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,9 @@
 import {lusitana} from '@/app/ui/fonts';
+import {fetchRevenue} from '../lib/data';
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 
 export default async function Page() {
+    const revenue = await fetchRevenue();
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -17,7 +20,7 @@ export default async function Page() {
         /> */}
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-                {/* <RevenueChart revenue={revenue}  /> */}
+                <RevenueChart revenue={revenue}/>
                 {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
             </div>
         </main>
