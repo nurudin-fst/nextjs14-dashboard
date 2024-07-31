2e41fcabac02e7c2f720cdc5bbed3adf728b6913
hendisantika <hendisantika@yahoo.co.id>
2024-07-31 18:49:04 +0700

    Modify dashboard page.tsx🫸🌀✏️📗🐧🐳

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -208,3 +208,6 @@ export async function fetchFilteredCustomers(query: string) {
     throw new Error('Failed to fetch customer table.');
   }
 }
+
+const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
+const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
