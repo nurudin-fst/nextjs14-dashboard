6daeaecc4622813a8c9e54c4a951683bc57ea794
hendisantika <hendisantika@yahoo.co.id>
2024-08-03 00:16:39 +0700

    Bug Fix Login Action🫸🌀✏️📗🐧🐳⬆

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,20 +1,18 @@
 'use client';
 
+import {authenticate} from '@/app/lib/actions';
 import {lusitana} from '@/app/ui/fonts';
 import {AtSymbolIcon, ExclamationCircleIcon, KeyIcon,} from '@heroicons/react/24/outline';
 import {ArrowRightIcon} from '@heroicons/react/20/solid';
 import {Button} from './button';
-import {authenticate} from "@/app/lib/actions";
-import {useActionState} from "react";
+import {useFormState, useFormStatus} from 'react-dom';
 
 export default function LoginForm() {
-  const [errorMessage, formAction, isPending] = useActionState(
-      authenticate,
-      undefined,
-  );
+  const [code, action] = useFormState(authenticate, undefined);
+  const {pending} = useFormStatus();
 
   return (
-      <form action={formAction} className="space-y-3">
+      <form action={action} className="space-y-3">
         <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
           <h1 className={`${lusitana.className} mb-3 text-2xl`}>
             Please log in to continue.
@@ -62,22 +60,28 @@ export default function LoginForm() {
               </div>
             </div>
           </div>
-          <Button className="mt-4 w-full" aria-disabled={isPending}>
-            Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50"/>
-          </Button>
-          <div
-              className="flex h-8 items-end space-x-1"
-              aria-live="polite"
-              aria-atomic="true"
-          >
-            {errorMessage && (
+          <LoginButton/>
+          <div className="flex h-8 items-end space-x-1">
+            {code === 'CredentialsSignin' && (
                 <>
                   <ExclamationCircleIcon className="h-5 w-5 text-red-500"/>
-                  <p className="text-sm text-red-500">{errorMessage}</p>
+                  <p aria-live="polite" className="text-sm text-red-500">
+                    Invalid credentials
+                  </p>
                 </>
             )}
           </div>
         </div>
       </form>
+  );
+}
+
+function LoginButton() {
+  const {pending} = useFormStatus();
+
+  return (
+      <Button className="mt-4 w-full" aria-disabled={pending}>
+        Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50"/>
+      </Button>
   );
 }
