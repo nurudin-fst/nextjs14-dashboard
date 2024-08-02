0cadb137854dfadaa5e2b10e9bb90013e1d5e893
hendisantika <hendisantika@yahoo.co.id>
2024-08-02 17:33:26 +0700

    Add getUser🫸🌀✏️📗🐧🐳⬆

--- auth.ts
+++ auth.ts
@@ -1,7 +1,8 @@
-import NextAuth from 'next-auth';
+import NextAuth, {User} from 'next-auth';
 import {authConfig} from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
 import {z} from "zod";
+import {sql} from "@vercel/postgres";
 
 export const {auth, signIn, signOut} = NextAuth({
     ...authConfig,
@@ -15,3 +16,13 @@ export const {auth, signIn, signOut} = NextAuth({
         }),
     ],
 });
+
+async function getUser(email: string): Promise<User | undefined> {
+    try {
+        const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
+        return user.rows[0];
+    } catch (error) {
+        console.error('Failed to fetch user:', error);
+        throw new Error('Failed to fetch user.');
+    }
+}
