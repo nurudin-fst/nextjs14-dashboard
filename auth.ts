af5669762111d02f739959e5f65c9cb81f16d900
hendisantika <hendisantika@yahoo.co.id>
2024-08-02 17:34:58 +0700

    Modify auth.ts🫸🌀✏️📗🐧🐳⬆

--- auth.ts
+++ auth.ts
@@ -3,6 +3,7 @@ import {authConfig} from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
 import {z} from "zod";
 import {sql} from "@vercel/postgres";
+import bcrypt from "bcrypt";
 
 export const {auth, signIn, signOut} = NextAuth({
     ...authConfig,
@@ -12,6 +13,17 @@ export const {auth, signIn, signOut} = NextAuth({
                 const parsedCredentials = z
                     .object({email: z.string().email(), password: z.string().min(6)})
                     .safeParse(credentials);
+                if (parsedCredentials.success) {
+                    const {email, password} = parsedCredentials.data;
+                    const user = await getUser(email);
+                    if (!user) return null;
+                    const passwordsMatch = await bcrypt.compare(password, user.password);
+
+                    if (passwordsMatch) return user;
+                }
+
+                console.log('Invalid credentials');
+                return null;
             },
         }),
     ],
