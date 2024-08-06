38a0425dea6055e8ceaf0e9acb49abdeefee7cdf
hendisantika <hendisantika@yahoo.co.id>
2024-08-06 07:07:53 +0700

    Bug Fix Login Action :three:🫸🌀✏️📗🐧🐳⬆

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,18 +1,17 @@
 'use client';
 
-import {authenticate} from '@/app/lib/actions';
 import {lusitana} from '@/app/ui/fonts';
 import {AtSymbolIcon, ExclamationCircleIcon, KeyIcon,} from '@heroicons/react/24/outline';
 import {ArrowRightIcon} from '@heroicons/react/20/solid';
 import {Button} from './button';
 import {useFormState, useFormStatus} from 'react-dom';
+import {authenticate} from '@/app/lib/actions';
 
 export default function LoginForm() {
-  const [code, action] = useFormState(authenticate, undefined);
-  const {pending} = useFormStatus();
+  const [errorMessage, dispatch] = useFormState(authenticate, undefined);
 
   return (
-      <form action={action} className="space-y-3">
+      <form action={dispatch} className="space-y-3">
         <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
           <h1 className={`${lusitana.className} mb-3 text-2xl`}>
             Please log in to continue.
@@ -61,13 +60,15 @@ export default function LoginForm() {
             </div>
           </div>
           <LoginButton/>
-          <div className="flex h-8 items-end space-x-1">
-            {code === 'CredentialsSignin' && (
+          <div
+              className="flex h-8 items-end space-x-1"
+              aria-live="polite"
+              aria-atomic="true"
+          >
+            {errorMessage && (
                 <>
                   <ExclamationCircleIcon className="h-5 w-5 text-red-500"/>
-                  <p aria-live="polite" className="text-sm text-red-500">
-                    Invalid credentials
-                  </p>
+                  <p className="text-sm text-red-500">{errorMessage}</p>
                 </>
             )}
           </div>
