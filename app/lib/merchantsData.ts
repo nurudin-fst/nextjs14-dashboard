0d8c23aadda0476f01b3f07d8b1b16711001ffd8
kenan-fst <kenanfaresta11@gmail.com>
2024-08-11 22:44:16 +0700

    clear hook useState and useEffect

--- app/lib/merchantsData.ts
+++ app/lib/merchantsData.ts
@@ -1,33 +1,13 @@
 'use client'
-import {useEffect, useState} from "react";
 
 const ITEMS_PER_PAGE = 10;
 
 export async function fetchMerchantPages(query: string) {
     try {
-        //       const count = await sql`SELECT COUNT(*)
-        //   FROM invoices
-        //   JOIN customers ON invoices.customer_id = customers.id
-        //   WHERE
-        //     customers.name ILIKE ${`%${query}%`} OR
-        //     customers.email ILIKE ${`%${query}%`} OR
-        //     invoices.amount::text ILIKE ${`%${query}%`} OR
-        //     invoices.date::text ILIKE ${`%${query}%`} OR
-        //     invoices.status ILIKE ${`%${query}%`}
-        // `;
         let count;
-        const [data, setData] = useState([]);
-
-        useEffect(() => {
-            async function fetchData() {
-                const response = await fetch('https://61a0ea8a6c3b400017e69ae8.mockapi.io/api/v1/users/merchants');
-                const result = await response.json();
-                setData(result);
-            }
-
-            fetchData();
-        }, []);
-        count = data.length;
+        const response = await fetch('https://61a0ea8a6c3b400017e69ae8.mockapi.io/api/v1/users/merchants');
+        const result = await response.json();
+        count = result.length;
 
         const totalPages = Math.ceil(Number(count) / ITEMS_PER_PAGE);
         return totalPages;
